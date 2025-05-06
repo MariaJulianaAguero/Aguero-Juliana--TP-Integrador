3be39d7c84202ee5e22cfe2ccf6335b79bf3f49f
MariaJulianaAguero <110269481+MariaJulianaAguero@users.noreply.github.com>
2025-05-05 21:23:37 -0300

    Cambios en las rutas

--- server/server.js
+++ server/server.js
@@ -5,57 +5,60 @@ const cors = require('cors');
 
 const app = express();
 
-// Servir archivos estáticos desde la carpeta 'public'
-app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 3000;
+// Servir archivos estáticos desde la carpeta 'public'
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 
 app.post('/partidas', (req, res) => {
   const nueva = req.body;
 
   // Leer y parsear el ranking
-  const file = path.join(__dirname, 'ranking.json');
+  const file = path.join(__dirname, 'rankingData.json');
+  let ranking = [];
   if (fs.existsSync(file)) {
     ranking = JSON.parse(fs.readFileSync(file, 'utf8'));
   }
 
-  // Agregar, ordenar y recortar
   ranking.push(nueva);
+
   ranking.sort((a, b) => {
     if (b.puntaje !== a.puntaje) return b.puntaje - a.puntaje;
     return a.tiempoTotal - b.tiempoTotal;
   });
-  const top20 = ranking.slice(0, 20);
 
-  
+  const top20 = ranking.slice(0, 20);
   fs.writeFileSync(file, JSON.stringify(top20, null, 2));
 
-  // Va a devolver un archivo JSON con el ranking actualizado
   res.status(201).json({ message: 'Partida guardada', top20 });
 });
 
 app.get('/ranking', (req, res) => {
-  const file = path.join(__dirname, 'ranking.json');
-  if (fs.existsSync(file)) {
-    const ranking = JSON.parse(fs.readFileSync(file, 'utf8'));
-    res.json(ranking);
-  } else {
-    res.json([]);
-  }
-});
+  const file = path.join(__dirname, 'rankingData.json');
 
-// Ruta para servir el archivo index.html
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: 'No se pudo leer el archivo ranking.json' });
+    }
+
+    try {
+      const ranking = JSON.parse(data); // Asegúrate de que el archivo ranking.json esté bien formado
+      res.json(ranking);
+    } catch (err) {
+      return res.status(500).json({ error: 'Error al procesar los datos del ranking' });
+    }
+  });
 });
 
 
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
+
 });
 
